ce6c32bafcecb691015f38519362c20b0e830bc5
DouglasHT <doug.wow@hotmail.com>
2019-04-25 12:24:32 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:procura_gifs/ui/gif_page.dart';
+import 'package:procura_gifs/ui/home_page.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.black),
+  ));
+}
+
+
+
